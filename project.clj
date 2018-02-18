e5da96e0961aa840a888dd4660986ed7a74a96d4
Jordan Jones <jrjh27cs@gmail.com>
2018-02-17 23:51:37 -0600

    Indenting

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject test-env "0.1.0-Snapshot"
-:description "FIXME"
-:url "https://github.com/Jrjh27CS/test-env"
-:dependencies [[org.clojure/clojure "1.9.0"]]
-:main test-env.core)
+  :description "FIXME"
+  :url "https://github.com/Jrjh27CS/test-env"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main test-env.core)
